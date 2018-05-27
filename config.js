035eab7b986cb33f83fb8ce596ee84a22750bb44
John Joo <john@Johns-iMac.local>
2018-05-27 15:21:06 -0700

    added test url

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/reblog-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                      'mongodb://localhost/testreblog-app';
+                      'mongodb://localhost/test-reblog-app';
 exports.PORT = process.env.PORT || 8080;
